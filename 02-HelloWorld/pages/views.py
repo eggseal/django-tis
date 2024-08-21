26e11840e1f7353895eba310f39e235d40d25181
eggseal <nicosm2004@gmail.com>
2024-08-20 20:24:14 -0500

    Product creation

--- 02-HelloWorld/pages/views.py
+++ 02-HelloWorld/pages/views.py
@@ -57,16 +57,17 @@ class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
     context_object_name = 'products' # This will allow you to loop through 'products' in your template
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Products - Online Store'
         context['subtitle'] = 'List of products'
         return context
     
-class ProductForm(forms.Form):
-    name = forms.CharField(required=True)
-    price = forms.FloatField(required=True)
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
@@ -86,14 +87,7 @@ class ProductCreateView(View):
     def post(self, request):
         form = ProductForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            price = form.cleaned_data['price']
-            Product.products.append({
-                "id": len(Product.products) + 1,
-                "name": name,
-                "price": price,
-                "description": "One of the products of all time"
-            })
+            form.save()
             return HttpResponseRedirect(reverse('success'))
         else:
             viewData = {}
