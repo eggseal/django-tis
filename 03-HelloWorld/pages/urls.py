396e3ec9ed29214abe187343dea4695ccc17f74c
eggseal <nicosm2004@gmail.com>
2024-08-28 08:46:19 -0500

    Added cart

--- 03-HelloWorld/pages/urls.py
+++ 03-HelloWorld/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ContactPageView, ProductSuccessView
+from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ContactPageView, ProductSuccessView, CartView, CartRemoveAllView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -9,4 +9,7 @@ urlpatterns = [
     path('products/success', ProductSuccessView.as_view(), name='success'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
     path('contacts/', ContactPageView.as_view(), name='contacts'),
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
 ]
